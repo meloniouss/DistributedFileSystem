28831b43692ec106cc8aedf8bcaa7de0f2cccea5
meloniouss <andrei.jianu31@gmail.com>
2025-07-09 21:19:06 -0400

    Add Thread-safe Queue Data Structure

---
+++ ThreadSafeQueue.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <queue>
+#include <mutex>
+
+template<typename T>
+class ThreadSafeQueue{
+public:
+	void push(const T& value){
+		std::lock_guard<std::mutex> lock(mtx);
+		q.push(value);
+		cv.notify_one();
+	}
+	void pop(){
+		std::lock_guard<std::mutex> lock(mtx);
+		q.pop();
+	}
+
+	void wait_and_pop(T& out){
+		std::unique_lock<std::mutex> lock(mtx);
+		cv.wait(lock, [this]{return !q.empty();});
+		out = std::move(q.front());
+		q.pop();
+	}
+
+	size_t size() const{
+		std::lock_guard<std::mutex> lock(mtx);
+		return q.size();
+	}
+	bool empty() const{
+		std::lock_guard<std::mutex> lock(mtx);
+		return q.empty();
+	}
+private:
+	std::queue<T> q;
+	std::condition_variable cv;
+	mutable std::mutex mtx;
+};
